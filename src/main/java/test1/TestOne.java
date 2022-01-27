f380485ecbebaf244c385796fbb8f2e85560388f
myicskt <myicskt@gmail.com>
2022-01-26 19:29:27 -0600

    first comm

---
+++ src/main/java/test1/TestOne.java
@@ -0,0 +1,30 @@
+package test1;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestOne {
+
+	@Test
+	public void init(){
+		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://techfios.com/test/101/");
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.findElement(By.name("allbox")).click();
+		
+		WebElement checkboxall = driver.findElement(By.name("allbox"));
+		if (checkboxall.isSelected() == false) {
+			System.out.println("Toggle all  is checked ");
+		}
+		
+		driver.close();
+			
+	}
+
+}
